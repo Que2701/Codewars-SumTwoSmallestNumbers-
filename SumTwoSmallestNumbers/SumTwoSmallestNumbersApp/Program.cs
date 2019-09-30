db2d403acb633e033ccef3c9e230326582d8358b
tumelo <tumelom@evolvemed.co.za>
2019-09-30 13:14:41 +0200

    Sum two smallest integers

---
+++ SumTwoSmallestNumbers/SumTwoSmallestNumbersApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumTwoSmallestNumbersApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 19, 5, 42, 2, 77 };
+            Console.WriteLine(sumTwoSmallestNumbers(numbers));
+            Console.WriteLine(sumTwoSmallestNumbersII(numbers));
+        }
+        static int sumTwoSmallestNumbers(int[] numbers)
+        {
+            // sort the array 
+            var data = numbers.OrderBy(x => x).ToArray();
+            return data[0] + data[1];
+        }
+
+        static int sumTwoSmallestNumbersII(int[] numbers)
+        {
+            return numbers.OrderBy(x => x).Take(2).Sum();
+        }
+    }
+}
